cab6519eda603aead35365febc0ba598393d7ea3
bugrabalim11 <bugrablm11@gmail.com>
2026-03-10 00:30:27 +0300

    MVC projesine Bootstrap ile modern ürün vitrini (Index) eklendi. _Layout yapısı güncellendi ve CreateProduct (Ürün Ekleme) altyapısı DTO'lar ile birlikte hazırlandı.

---
+++ ECommerce.MVC/DTOs/ProductDtos/CreateProductDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ECommerce.MVC.DTOs.ProductDtos
+{
+    public class CreateProductDto
+    {
+        public string ProductName { get; set; } = string.Empty;
+        public decimal ProductPrice { get; set; }
+        public string İmageUrl { get; set; } = string.Empty;
+        public int CategoryId { get; set; }
+    }
+}
